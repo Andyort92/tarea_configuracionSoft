d9b44f0aa38274cb8cc75f8b32afdb325253292f
Andyort92 <168473868+Andyort92@users.noreply.github.com>
2025-06-29 20:16:33 -0500

    Conflicto resuelto

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ let count = 0;
 
 function increase() {
     count++;
-    document.getElementById('counter').innerText = count;
+    document.getElementById('counter1').innerText = count;
 }
 
 function decrease() {
     count--;
-    document.getElementById('counter').innerText = count;
+    document.getElementById('counter1').innerText = count;
 }
 
 function reset() {
     count = 0;
-    document.getElementById('counter').innerText = count;
+    document.getElementById('counter1').innerText = count;
 }
